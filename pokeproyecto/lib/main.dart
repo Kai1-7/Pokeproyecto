a914dec86d9112a39dea76ba05c2fd2da973f778
willian0521 <carbajalwillian12@gmail.com>
2025-06-05 01:55:02 -0600

    LOGIN FIXED FOR WEB (SIGNINWITHGOOGLE) Y ANDROID. HOME REDONE. ADDED LANDING TO MAIN.DART (TO DECIDE WHAT PAGE TO CHARGE DEPENDING ON AUTH STATE). ROUTES ADDED. NOT TESTED YET.

--- pokeproyecto/lib/main.dart
+++ pokeproyecto/lib/main.dart
@@ -2,75 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'auth/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
-  runApp(const Home());
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Pokeproyecto',
       initialRoute: '/login',
       routes: {
-        '/login': (context) => Login(), // El nombre puede ser el que tú decidas
-        '/home': (context) => Home(), // Supongamos que luego haces esta
+        '/login': (context) => Login(), //Pantalla de login
+        '/home': (context) => Home(), //Esta pantalla
       },
-      title: 'Pokeproyecto',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
-  final String title;
+  final String title = "Home";
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("Pokemon"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        actions: [
+          //botones
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: const Center(child: Text("Home page")),
+    );
+  }
+}
+
+class Landing extends StatelessWidget {
+  const Landing({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      //StreamBuilder porque lo que construyamos depende de un stream
+      stream:
+          FirebaseAuth.instance
+              .authStateChanges(), //Depende de esto (el estado de autentificación)
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasData) {
+          //Si lo que devuelve el stream no es nulo
+          return const Home();
+        } else {
+          //si es nulo vuelve a login
+          return const Login();
+        }
+      },
     );
   }
 }
